a1facd88e91aa7ba45f204852d93941e6d226f54
Nikolay <kolunya0@gmail.com>
2023-05-08 18:32:54 +0300

    финальные изменения

--- insert.py
+++ insert.py
@@ -12,16 +12,18 @@ create_table(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-with open('tests_data.json', 'r') as fd:
-    data = json.load(fd)
 
-for record in data:
-    model = {
-        'publisher': Publisher,
-        'shop': Shop,
-        'book': Book,
-        'stock': Stock,
-        'sale': Sale,
-    }[record.get('model')]
-    session.add(model(id=record.get('pk'), **record.get('fields')))
-session.commit()
+def record_database():
+    with open('tests_data.json', 'r') as fd:
+        data = json.load(fd)
+
+    for record in data:
+        model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale,
+        }[record.get('model')]
+        session.add(model(id=record.get('pk'), **record.get('fields')))
+    session.commit()
--- main.py
+++ main.py
@@ -1,77 +1,40 @@
+import psycopg2
 import sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
-
-Base = declarative_base() # Создание класса из библиотеки SQLAlchemy
-
-
-
-# Создаем класс издателей
-class Publisher(Base):
-    __tablename__ = 'publisher'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(length=60), unique=True, nullable=False)
-
-
-
-# Создаем класс книг
-class Book(Base):
-    __tablename__ = 'book'
-
-    id = Column(Integer, primary_key=True)
-    title = Column(String(length=60), unique=True, nullable=False)
-    publisher_id = Column(Integer, ForeignKey('publisher.id'))
-
-    publisher = relationship(Publisher, backref='book')
-
-
-
-# Создаем класс магазинов
-class Shop(Base):
-    __tablename__ = 'shop'
-
-    id = Column(Integer, primery_key=True)
-    shop_name = Column(String(length=60), nullable=False)
-
-
-
-# Создаем класс Товарного запаса
-class Stock(Base):
-    __tablename__ = 'stock'
-
-    id = Column(Integer, primery_key=True)
-    book_id = Column(Integer, ForeignKey('book.id'))
-    shop_id = Column(Integer, ForeignKey('shop.id'))
-    count = Column(Integer, nullable=False)
-
-    book = relationship(Book, backref='stock')
-    shop = relationship(Shop, backref='stock')
-
-
-
-# Создаем класс продаж
-class Sale(Base):
-    __tablename__ = 'sale'
-
-    id = Column(Integer, primery_key=True)
-    price = Column(Integer, nullable=False)
-    date_sale = Column(DateTime, nullable=False)
-    stock_id = Column(Integer, ForeignKey('stock.id'))
-    count_sale = Column(Integer, nullable=False)
-
-    stock = relationship(Stock, backref='sale')
-
-
-# Создаем функцию
-def create_tables(engine):
-    Base.metadata.cteate_all(engine) # Так же можно удалить все - Base.metadata.drop_all(engine)
+from sqlalchemy.orm import sessionmaker
+from models import create_table
+from insert import record_database
+from models import Publisher, Book, Shop, Stock, Sale
 
 DSN = 'postgresql://postgres:1604@localhost:5432/orm'
 engine = sqlalchemy.create_engine(DSN)
+create_table(engine)
+
+record_database()  # Функция наполнения базы данных из json файла
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-session.close()
+# Получение имени издателя от пользователя
+publisher_name = input('Введите имя издателя: ')
 
+# Получение издателя из базы данных
+publisher = session.query(Publisher).filter_by(name=publisher_name).first()
+
+# Проверка, что издатель найден
+if not publisher:
+    print(f'Издатель с именем "{publisher_name}" не найден')
+else:
+    # Получение фактов покупки книг для данного издателя
+    sales = session.query(Sale) \
+        .join(Stock) \
+        .join(Book) \
+        .join(Publisher) \
+        .join(Shop) \
+        .filter(Publisher.id == publisher.id) \
+        .all()
+
+    # Вывод фактов покупки книг
+    for sale in sales:
+        print(f'{sale.stock.book.title} | {sale.stock.shop.name} | {sale.price} | {sale.date_sale}')
+
+session.close()
